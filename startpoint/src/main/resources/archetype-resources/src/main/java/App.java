62ea0014ac37ef5ff55e090c5c1bce16631939e8
jay <yang6n@gmail.com>
2013-04-20 23:57:58 -0500

    Initial Import.

---
+++ startpoint/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,23 @@
+/*
+ * -----------------------------------------------------------------
+ * Copyright (c) 2013 FanStudio, All Rights Reserved.
+ * This software is the proprietary information of FanStudio,
+ * Use is subject to strict licensing terms.
+ * -----------------------------------------------------------------
+ */
+
+package $package;
+
+/**
+ * Hello world!
+ * @author jay
+ * @version 1.0 - Apr 20, 2013
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
